ad6ddd52747aba6702f8fe1bc7f14d317cb64020
Laurent Almeras <lalmeras@gmail.com>
2018-01-24 17:01:48 +0100

    updated bindgen, dropped toolchain 1.7, reworked base AbstractCoreBinding

--- src/main/java/org/iglooproject/commons/util/binding/AbstractCoreBinding.java
+++ src/main/java/org/iglooproject/commons/util/binding/AbstractCoreBinding.java
@@ -1,13 +1,33 @@
 package org.iglooproject.commons.util.binding;
 
+import org.bindgen.BindingRoot;
+import org.bindgen.Getter;
+import org.bindgen.Setter;
 import org.bindgen.binding.AbstractBinding;
 
 import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
 
-public abstract class AbstractCoreBinding<R, T> extends AbstractBinding<R, T> implements ICoreBinding<R, T> {
+public class AbstractCoreBinding<R, P, T> extends AbstractBinding<R, P, T> implements ICoreBinding<R, T> {
 
 	private static final long serialVersionUID = 6398238108193037144L;
+
+	public AbstractCoreBinding() {
+		super();
+	}
+
+	public AbstractCoreBinding(Class<?> type) {
+		super(null, type, null, null, null);
+	}
+
+	public AbstractCoreBinding(String name, BindingRoot<R, P> parentBinding, Getter<P, T> getter, Setter<P, T> setter) {
+		super(name, parentBinding, getter, setter);
+	}
+
+	public AbstractCoreBinding(String name, Class<?> type, BindingRoot<R, P> parentBinding, Getter<P, T> getter,
+			Setter<P, T> setter) {
+		super(name, type, parentBinding, getter, setter);
+	}
 
 	@Override
 	public T apply(R input) {
